5d92264b41c84012f9aa2081ab6dc253289d1adb
Bryan Hunt <sentimental.bryan@gmail.com>
2012-06-19 12:14:15 +0100

    mavenized, checked in the pom etc.

---
+++ DEVELOPER.rst
@@ -0,0 +1,18 @@
+For a local test server, I use bottle-websocket and websocket-client python packages.
+
+```
+sudo pip install bottle-websocket
+sudo pip install websocket-client
+```
+
+They can be found in the resources directory.
+
+A test client to verify everything is working ok
+
+websocket_client.py
+
+A test server for use when running integration tests.
+
+websocket_example.py
+
+
---
+++ resources/websocket_example.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+import json
+from bottle import route, run, request, abort, Bottle ,static_file
+from pymongo import Connection
+from gevent import monkey; monkey.patch_all()
+from time import sleep
+
+app = Bottle()
+
+@app.route('/websocket')
+def handle_websocket():
+    wsock = request.environ.get('wsgi.websocket')
+    if not wsock:
+        abort(400, 'Expected WebSocket request.')
+    while True:
+        try:
+            message = wsock.receive()
+            wsock.send("Your message was: %r" % message)
+            sleep(3)
+            wsock.send("Your message was: %r" % message)
+        except WebSocketError:
+            break
+
+@app.route('/<filename:path>')
+def send_html(filename):
+    return static_file(filename, root='./static', mimetype='text/html')
+
+
+from gevent.pywsgi import WSGIServer
+from geventwebsocket import WebSocketHandler, WebSocketError
+
+host = "127.0.0.1"
+port = 8080
+
+server = WSGIServer((host, port), app,
+                    handler_class=WebSocketHandler)
+print "access @ http://%s:%s/websocket.html" % (host,port)
+server.serve_forever()
